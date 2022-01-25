1d07416c4902477350b4343ed29643e84a02628a
Florian <florianjeandenans@gmail.com>
2022-01-25 19:57:08 +0100

    fix error on socket connexion

--- Server/app.js
+++ Server/app.js
@@ -59,9 +59,11 @@ io.on('connection', (socket) => {
         // console.log(socket.handshake);
 
         console.log(data);
-        console.log(data["user"]);
+        console.log(data.type);
+        console.log(data.user);
         console.log(data.token);
-        // console.log(JSON.parse(data).token);
+
+        // console.log(JSON.stringify(data).token);
         token = data.token;
         jwt.verify(data.user, process.env.TOKEN_KEY, (err, decoded) => {
             if(err) {
