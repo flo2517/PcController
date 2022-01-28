7e699a231a077bc3fca8c7f54786947392f3a66c
Florian <florianjeandenans@gmail.com>
2022-01-28 11:09:48 +0100

    add template for verify mail

--- Server/src/api/services/email.service.js
+++ Server/src/api/services/email.service.js
@@ -1,26 +1,30 @@
 const transporter = require('../../config/mailer.config');
+const ejs = require('ejs');
 
 class EmailService {
     
 }
 
 EmailService.sendVerifyMail = (email, verifyString) => {
-    let sender = process.env.MAIL_USER;
-    let mailOptions = {
-        from: sender,
-        to: email,
-        subject: 'Verify your email',
-        text: 'Verify your email',
-        html: `<p>Please verify your email by clicking the link below:</p>
-        <a href="http://${process.env.API_HOST}:${process.env.API_PORT}/verify/${verifyString}">Verify</a>`
-    };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' , info);
-        }
+    let sender = process.env.MAIL_USER;
+    ejs.renderFile(__dirname + '/../../views/emails/welcomeEmail.ejs', {
+        user_firstname: 'You',
+        confirm_link: `http://${process.env.API_HOST}:${process.env.API_PORT}/verify/${verifyString}"`
+    }).then(function(data) {
+        let mailOptions = {
+            from: sender,
+            to: email,
+            subject: 'Welcome to the app',
+            html: data
+        };
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
     });
 }
 
