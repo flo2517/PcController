2c880ad0858e9823510cf185c682a76e8b778264
Florian <florianjeandenans@gmail.com>
2022-02-06 16:23:14 +0100

    Fix refreshToken

--- Server/src/api/controllers/auth.controller.js
+++ Server/src/api/controllers/auth.controller.js
@@ -186,7 +186,7 @@ const refreshToken = (req, res) => {
                 id: user.id,
                 email: user.email,
                 username: user.username
-            }, process.env.TOkEN_KEY, {
+            }, process.env.TOKEN_KEY, {
                 expiresIn: process.env.JWT_EXPIRATION_TIME
             });
             return res.status(200).json({
