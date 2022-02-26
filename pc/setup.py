9b5d017d280e234d8c9b44fbf54e5fea73a522b0
Chounes <maxride445@gmail.com>
2022-02-26 18:21:46 +0100

    Adding linux executable

--- pc/setup.py
+++ pc/setup.py
@@ -1,15 +1,25 @@
 from cx_Freeze import setup, Executable
 import sys
 
-# base="Win32GUI" should be used only for Windows GUI app
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
 
 
 setup(
     name="Premote",
     version="1.0",
     description="Pc remote controller software",
-    executables=[Executable("Main.py", base=base)],
+    executables=[Executable("Main.py", base=None)],
 )
+
+if sys.platform == "win32":
+    packages = ["os", "requests", "tk", "pillow", "json", "uuid", "python-socketio", "pystray", "sys", "re",
+                "threading", "multiprocessing", "pycryptodome", "PyAutoGui"]
+    build_exe_options = {"packages": packages, "excludes": [""]}
+    setup(
+        name="Premote",
+        version="1.0",
+        description="Pc remote controller software",
+        options={"build_exe": build_exe_options},
+        executables=[Executable("Main.py", base="Win32GUI")],
+    )
+
+
