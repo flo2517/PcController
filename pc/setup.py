fc8b24b398b8aec9b1d040af7097d57e684c1fed
Chounes <maxride445@gmail.com>
2022-02-27 06:40:52 +0100

    Add class diagram vpd file

--- pc/setup.py
+++ pc/setup.py
@@ -1,14 +1,9 @@
 from cx_Freeze import setup, Executable
 import sys
 
-packages = ["requests", "tk", "PIL", "json", "uuid", "socketio", "pystray", "threading", "multiprocessing", "pycryptodome"]
-build_exe_options = {"packages": packages, "excludes": []}
-
-
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
 if sys.platform == "win32":
-    packages.append("PyAutoGui")
     base = "Win32GUI"
 
 
@@ -16,6 +11,5 @@ setup(
     name="Premote",
     version="1.0",
     description="Pc remote controller software",
-    options={"build_exe": build_exe_options},
     executables=[Executable("Main.py", base=base)],
 )
