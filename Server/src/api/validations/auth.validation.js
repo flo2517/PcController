0d868be50773f85faec8a903c20a128691cb1054
Florian <florianjeandenans@gmail.com>
2022-03-01 18:33:04 +0100

    change regex

--- Server/src/api/validations/auth.validation.js
+++ Server/src/api/validations/auth.validation.js
@@ -1,11 +1,9 @@
 const validateEmail = (email) => {
-    //return String(email).match(/^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i);
-    return true;
+    return String(email).match(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Z|a-z]{2,}\b/i);
 };
 
 const validatePassword = (password) => {
-    // return String(password).match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/);
-    return password.length >= 8;
+    return String(password).match(/^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i);
 };
 
 module.exports = {
