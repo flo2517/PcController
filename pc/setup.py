6a0404bd694045aaad53bd5ed789f39979c1bc25
Chounes <maxride445@gmail.com>
2022-03-01 16:18:50 +0100

    Add exe file for windows and linux

--- pc/setup.py
+++ pc/setup.py
@@ -6,10 +6,11 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-
+include_files = {'include_files': 'pictures'}
 setup(
     name="Premote",
     version="1.0",
     description="Pc remote controller software",
+    options={'build_exe': include_files},
     executables=[Executable("Main.py", base=base)],
 )
