d8741f9cf482c0842dea612d671c408fcb78798b
Florian <florianjeandenans@gmail.com>
2022-03-07 21:25:35 +0100

    update the api docs

--- Server/src/config/openapi.config.js
+++ Server/src/config/openapi.config.js
@@ -3,8 +3,18 @@ const openapi = require("@wesleytodd/openapi");
 module.exports = openapi({
     openapi: '3.0.0',
     info: {
-        title: 'Express App',
-        description: 'Generated docs from an express api',
-        version: '1.0.0'
+        title: 'Pandapp API Documentation',
+        description: 'Generated docs from the pandapp-api project, you can find more about at [https://pandapp.thrallweb.fr](https://pandapp.thrallweb.fr) ',
+        version: '1.0.0',
+        contact: {
+            name: 'Website',
+            url: 'https://pandapp.thrallweb.fr'
+        },
+        servers: [
+            {
+                url: 'https://pandapp.thrallweb.fr/api/v1',
+                description: 'Production'
+            }
+        ]
     }
 });
