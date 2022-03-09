45fdfefa8611535a937788b2c9cc56be60c7426e
Chounes <maxride445@gmail.com>
2022-03-09 22:34:04 +0100

    Add rework all linux ui Add readme for linux users Add all command for linux Remain to do : - find way to use pil - Create linux exe

--- pc/linux/src/executors/LinuxExecutor.py
+++ pc/linux/src/executors/LinuxExecutor.py
@@ -13,7 +13,7 @@ class LinuxExe:
         if commandID == 0:
             return 0
         elif commandID == 1:
-            os.system("shutdown -s -t 1")
+            os.system("shutdown -h now")
             return 0
         elif commandID == 2:
             test = os.system("amixer -D pulse sset Master 10%+")
@@ -32,12 +32,24 @@ class LinuxExe:
             os.system("playerctl pause")
             return 0
         elif commandID == 7:
-            os.system("gnome-screensaver-command -l")
+            os.system("xdotool key super+l")
             return 0
         elif commandID == 8:
             os.system("playerctl next")
             return 0
         elif commandID == 9:
+            os.system("playerctl previous")
+            return 0
+        elif commandID == 10:
+            os.system("playerctl previous")
+            return 0
+        elif commandID == 11:
+            os.system("playerctl previous")
+            return 0
+        elif commandID == 12:
+            os.system("playerctl previous")
+            return 0
+        elif commandID == 13:
             os.system("playerctl previous")
             return 0
         else:
