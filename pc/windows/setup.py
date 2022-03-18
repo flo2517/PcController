e14332fe21adcb58d7e8022e0bbf4609efbf00b5
Chounes <maxride445@gmail.com>
2022-03-18 16:23:07 +0100

    Add popup windows to show error on socket communication

--- pc/windows/setup.py
+++ pc/windows/setup.py
@@ -1,14 +1,13 @@
 from cx_Freeze import setup, Executable
-import sys
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI"
 
-include_files = {'include_files': 'pictures'}
+include_files = {'include_files': 'windows/pictures'}
 setup(
     name="Pandapp",
     version="1.1",
     description="Pc remote controller software",
     options={'build_exe': include_files},
-    executables=[Executable("Pandapp.py", base=base, icon="pictures/pandapp.ico")],
+    executables=[Executable("windows/Pandapp.py", base=base, icon="windows/pictures/pandapp.ico")],
 )
